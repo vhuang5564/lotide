70dd6fab91cc5fc50187ea4748f5f64c1af9aca4
vhuang5564 <vhuang5564@gmail.com>
2021-06-29 02:03:08 +0000

    added import/export modules and removed test codes

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(head) {
   if (head === '') {
@@ -14,10 +7,3 @@ const head = function(head) {
     return head[0];
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
